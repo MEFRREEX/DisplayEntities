1467b78b5bb288674e9f645312e5307e598f8681
Arkadiy <83061703+MEFRREEX@users.noreply.github.com>
2025-01-31 15:47:31 +0200

    fix: remove debug for allay and pnx

--- plugin-allay/src/main/java/com/mefrreex/displayentities/allay/AllayDisplayEntitiesPlugin.java
+++ plugin-allay/src/main/java/com/mefrreex/displayentities/allay/AllayDisplayEntitiesPlugin.java
@@ -1,15 +1,8 @@
 package com.mefrreex.displayentities.allay;
 
-import com.mefrreex.displayentities.api.entity.DisplayBlock;
-import com.mefrreex.displayentities.api.entity.DisplayEntityState;
-import com.mefrreex.displayentities.core.factory.*;
-import org.allaymc.api.block.data.BlockId;
-import org.allaymc.api.eventbus.EventHandler;
-import org.allaymc.api.eventbus.event.player.PlayerToggleSneakEvent;
 import org.allaymc.api.plugin.Plugin;
 import com.mefrreex.displayentities.api.DisplayEntitiesProvider;
 import com.mefrreex.displayentities.api.entity.DisplayEntity;
-import org.allaymc.api.server.Server;
 
 public class AllayDisplayEntitiesPlugin extends Plugin {
 
@@ -18,27 +11,5 @@ public class AllayDisplayEntitiesPlugin extends Plugin {
     @Override
     public void onEnable() {
         DisplayEntitiesProvider.set(new AllayDisplayEntities(this));
-        Server.getInstance().getEventBus().registerListener(this);
-    }
-
-    @EventHandler
-    public void onSkean(PlayerToggleSneakEvent event) {
-        if (!event.isValue()) {
-            return;
-        }
-
-        DisplayEntityState state = DisplayEntityStateFactory.builder()
-                .entityPosition(EntityPositionFactory.builder()
-                        .x((float) event.getPlayer().getLocation().x())
-                        .y((float) event.getPlayer().getLocation().y())
-                        .z((float) event.getPlayer().getLocation().z())
-                        .build())
-                .position(PositionFactory.empty())
-                .basePosition(BasePositionFactory.empty())
-                .rotation(RotationFactory.empty())
-                .scale(ScaleFactory.ofScale(1f))
-                .build();
-        DisplayEntity displayEntity = DisplayEntityFactory.createBlock(DisplayBlock.of("minecraft:redstone_lamp", 1), state);
-        DisplayEntitiesProvider.get().getDisplayEntityManager().show(displayEntity, event.getPlayer().getUUID());
     }
 }
